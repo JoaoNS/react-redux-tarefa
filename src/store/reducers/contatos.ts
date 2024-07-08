eba548a2bdf77109ead83e78df4df3a25e9aeaa8
JoaoNS <joaopnunes252@gmail.com>
2024-07-08 14:34:42 -0300

    Initial commit

---
+++ src/store/reducers/contatos.ts
@@ -0,0 +1,68 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import ContatoClass from '../../models/Contato'
+
+type ContatosState = {
+  itens: ContatoClass[]
+}
+
+const initialState: ContatosState = {
+  itens: [
+    {
+      id: 1,
+      nome: 'Name Example',
+      email: 'name@gmail.com',
+      telefone: '00-000000000'
+    },
+    {
+      id: 2,
+      nome: 'Name Example',
+      email: 'name@gmail.com',
+      telefone: '00-000000000'
+    },
+    {
+      id: 3,
+      nome: 'Name Example',
+      email: 'name@gmail.com',
+      telefone: '00-000000000'
+    },
+    {
+      id: 4,
+      nome: 'Name Example',
+      email: 'name@gmail.com',
+      telefone: '00-000000000'
+    }
+  ]
+}
+const contatosSlice = createSlice({
+  name: 'contatos',
+  initialState,
+  reducers: {
+    deletar: (state, action: PayloadAction<number>) => {
+      state.itens = state.itens.filter(
+        (contato) => contato.id !== action.payload
+      )
+    },
+    editar: (state, action: PayloadAction<ContatoClass>) => {
+      const indexDoContato = state.itens.findIndex(
+        (c) => c.id === action.payload.id
+      )
+      if (indexDoContato >= 0) {
+        state.itens[indexDoContato] = action.payload
+      }
+    },
+    cadastrar: (state, action: PayloadAction<ContatoClass>) => {
+      const contatoExistente = state.itens.find(
+        (contato) =>
+          contato.nome.toLowerCase() === action.payload.nome.toLowerCase()
+      )
+      if (contatoExistente) {
+        alert('Contato já cadastrado')
+      } else {
+        state.itens.push(action.payload)
+      }
+    }
+  }
+})
+
+export const { deletar, editar, cadastrar } = contatosSlice.actions
+export default contatosSlice.reducer
